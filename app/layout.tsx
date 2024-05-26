0e3a6a55eea745e5b1aac49514ee4d4615b2f4b1
Julien Leveneur <jleveneur.pro@gmail.com>
2024-05-26 19:29:50 +0000

    feat: Update RootLayout to include Header component

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter as FontSans } from 'next/font/google';
 import './globals.css';
 import { cn } from '@/lib/utils';
 import { ThemeProvider } from '@/components/theme-provider';
+import Header from '@/components/shared/Header';
 
 const fontSans = FontSans({
   subsets: ['latin'],
@@ -20,7 +21,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
+    <html lang='en' suppressHydrationWarning={true}>
       <body
         className={cn('min-h-screen font-sans antialiased', fontSans.variable)}
       >
@@ -30,7 +31,15 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          {children}
+          <main className='flex min-h-screen w-full flex-col lg:flex-row'>
+            <Header />
+
+            <div className='mt-16 flex-1 overflow-hidden py-8 lg:max-h-screen lg:py-10'>
+              <div className='max-w-5xl mx-auto px-5 md:px-10 w-full'>
+                {children}
+              </div>
+            </div>
+          </main>
         </ThemeProvider>
       </body>
     </html>
