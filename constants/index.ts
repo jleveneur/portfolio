ce2f3c4ba8fe006f1576b419f0d7dd2c7b5c9875
Julien Leveneur <jleveneur.pro@gmail.com>
2024-05-26 18:00:20 +0000

    feat: Add navigation and social links constants

---
+++ constants/index.ts
@@ -0,0 +1,35 @@
+export const navLinks = [
+  {
+    label: 'Home',
+    route: '/',
+    icon: '/assets/icons/home.svg',
+  },
+  {
+    label: 'Projects',
+    route: '/projects',
+    icon: '/assets/icons/projects.svg',
+  },
+  {
+    label: 'About',
+    route: '/about',
+    icon: '/assets/icons/about.svg',
+  },
+  {
+    label: 'Contact',
+    route: '/contact',
+    icon: '/assets/icons/contact.svg',
+  },
+];
+
+export const socialLinks = [
+  {
+    label: 'Github',
+    url: 'https://github.com/jleveneur/portfolio',
+    icon: '/assets/icons/github.svg',
+  },
+  {
+    label: 'Linkedin',
+    url: 'https://www.linkedin.com/in/jleveneur/',
+    icon: '/assets/icons/linkedin.svg',
+  },
+];
