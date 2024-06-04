873a99a3cac5c9d8ce70eb9060123610a40161e4
Julien Leveneur <jleveneur.pro@gmail.com>
2024-06-04 21:14:41 +0000

    refactor: Update Home page layout and add Profile and TimelineSection components

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,23 @@
+import Profile from "@/components/shared/profile";
+import TimelineSection from "@/components/shared/timeline-section";
+import { education, experience } from "@/constants";
+
 export default function Home() {
-  return <section className="home">Home</section>;
+  return (
+    <div className="flex flex-col gap-20">
+      <Profile />
+      <TimelineSection
+        title="Éducation"
+        subtitle="Formation académique"
+        items={education}
+        isEducation={true}
+      />
+      <TimelineSection
+        title="Expérience"
+        subtitle="Parcours professionnel"
+        items={experience}
+        isEducation={false}
+      />
+    </div>
+  );
 }
