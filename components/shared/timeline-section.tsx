23b924f99b37f399a175ebd66066175bbec4fc29
Julien Leveneur <jleveneur.pro@gmail.com>
2024-06-04 14:11:27 +0000

    refactor: Update TimelineSection component styling for better readability

--- components/shared/timeline-section.tsx
+++ components/shared/timeline-section.tsx
@@ -17,7 +17,7 @@ const TimelineSection = ({
   return (
     <section className="grid max-w-6xl gap-4 sm:grid-cols-12">
       <div className="col-span-12 sm:col-span-3">
-        <div className="before:mb-5 before:block before:h-3 before:w-24 before:rounded-md before:dark:bg-primary">
+        <div className="before:mb-5 before:block before:h-3 before:w-24 before:rounded-md before:bg-primary">
           <h3 className="text-3xl font-semibold">{title}</h3>
           <span className="text-sm font-bold uppercase tracking-wider dark:text-gray-600">
             {subtitle}
@@ -25,11 +25,11 @@ const TimelineSection = ({
         </div>
       </div>
       <div className="relative col-span-12 space-y-6 sm:col-span-9 sm:px-4">
-        <div className="relative space-y-12 before:dark:bg-gray-300 sm:space-y-8 sm:px-4 sm:before:absolute sm:before:-left-3 sm:before:bottom-0 sm:before:top-2 sm:before:w-0.5">
+        <div className="relative space-y-12 before:bg-gray-300 sm:space-y-8 sm:px-4 sm:before:absolute sm:before:-left-3 sm:before:bottom-0 sm:before:top-2 sm:before:w-0.5">
           {items.reverse().map((item, index) => (
             <div
               key={index}
-              className="flex flex-col before:dark:bg-primary sm:relative sm:before:absolute sm:before:left-[-35px] sm:before:top-2 sm:before:z-[1] sm:before:h-4 sm:before:w-4 sm:before:rounded-full"
+              className="flex flex-col before:bg-primary sm:relative sm:before:absolute sm:before:left-[-35px] sm:before:top-2 sm:before:z-[1] sm:before:h-4 sm:before:w-4 sm:before:rounded-full"
             >
               <h3 className="text-xl font-semibold tracking-wide">
                 {isEducation
