802f1aa8812cfe2a7b60f74af5158377b584243e
Julien Leveneur <jleveneur.pro@gmail.com>
2024-06-05 22:55:57 +0000

    refactor: Add Skill type to types/index.d.ts

--- types/index.d.ts
+++ types/index.d.ts
@@ -29,3 +29,14 @@ export type ExperienceItem = {
 };
 
 export type TimelineItem = EducationItem | ExperienceItem;
+
+export type Skill = {
+  code?: string;
+  title: string;
+  category: string;
+  knowledge: string;
+  experience: string;
+  level: string;
+  type: string;
+  description?: string;
+};
